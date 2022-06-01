aa9e18572531c477c25520292b07e773ac680c22
kushagrasahani <96765538+kushagrasahani@users.noreply.github.com>
2022-06-02 00:29:27 +0530

    Update meslova.js

--- meslova.js
+++ meslova.js
@@ -10,15 +10,16 @@ homerel.addEventListener('click', () => {
     }, 500);
 });
 //used for relaoding the homepage on clicking the home icon
-const img = document.querySelectorAll('img')
-const imgArr = ['https://meslovawebsite-static.s3.amazonaws.com/img/demo/Home_Img_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/SensorData.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Text_to_data.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Audio_Text_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Satellite_img.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/LargeImages.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Data_image.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/FiberOptics.jpg']
-const counter = 0;
-function setImage() {
-    if (counter > imgArr.length) {
-        counter = 0;
-    }
-    img.src = imgArr[counter];
-    counter++;
-}
-const mytimer = setInterval(setImage, 3000);
+// const img = document.querySelectorAll('img')
+// const imgArr = ['https://meslovawebsite-static.s3.amazonaws.com/img/demo/Home_Img_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/SensorData.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Text_to_data.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Audio_Text_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Satellite_img.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/LargeImages.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Data_image.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/FiberOptics.jpg']
+// const counter = 0;
+// function setImage() {
+//     if (counter > imgArr.length) {
+//         counter = 0;
+//     }
+//     img.src = imgArr[counter];
+//     counter++;
+// }
+// const mytimer = setInterval(setImage, 3000);
 //this code is used for setting the image slider time
+document.querySelector('.carousel').carousel({ interval: 3000 });
