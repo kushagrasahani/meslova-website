8eef56ca09281df82db45c927d469ac32a2ea61a
kushagrasahani <96765538+kushagrasahani@users.noreply.github.com>
2022-06-03 00:01:49 +0530

    Update meslova.js

--- meslova.js
+++ meslova.js
@@ -1,29 +1,44 @@
+
+const loader = document.querySelector(".loader");
+window.addEventListener("load", function () {
+    loader.className += " hidden";
+});
+//to add preloading event on clickong the reloading button
 const rel = document.querySelector('.navbar-brand');
 rel.addEventListener('click', () => {
+    window.addEventListener("load", function () {
+        rel.className += " hidden";
+    });
     window.location.reload(true);
 });
-//used for for reloading the homepage on clicking the meslova icon
+//to reload the webpage on clicking meslova
 const homerel = document.querySelector('.nav-item');
 homerel.addEventListener('click', () => {
     setTimeout(() => {
-        window.location.reload(true)
+        window.addEventListener("load", function () {
+            homerel.className += " hidden";
+        })
+        window.location.reload(true);
     }, 500);
 });
-//used for relaoding the homepage on clicking the home icon
+//to reload the webpage on clicking home
+document.querySelector('.carousel').carousel({
+    interval: 3000
+});
+document.querySelector('.carousel').carousel('cycle');
+//event for moving image slider
+//observed that this code only runs when we click on next icon on the first image still figuring that out.
+
 // const img = document.querySelectorAll('img')
 // const imgArr = ['https://meslovawebsite-static.s3.amazonaws.com/img/demo/Home_Img_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/SensorData.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Text_to_data.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Audio_Text_1.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Satellite_img.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/LargeImages.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/Data_image.jpg', 'https://meslovawebsite-static.s3.amazonaws.com/img/demo/FiberOptics.jpg']
 // const counter = 0;
 // function setImage() {
-//     if (counter > imgArr.length) {
-//         counter = 0;
+//     for (let counter = 0; counter < imgArr.length; counter++) {
+//         img.src = imgArr[counter];
 //     }
-//     img.src = imgArr[counter];
-//     counter++;
-// }
 // const mytimer = setInterval(setImage, 3000);
-//this code is used for setting the image slider time
-// document.querySelector('.carousel').carousel({ interval: 3000 });
 
 
 
-$('.carousel').carousel({ interval: 2000 });
+
+
